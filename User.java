b40586ca6819334a066dd9c371df4812d0cf6093
Egan Satya Pricilio <87957664+EganSatya18@users.noreply.github.com>
2021-11-11 07:47:20 +0700

    UKL2

---
+++ User.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cobalelang2;
+
+public interface User {
+    public void setNama(String nama);
+    public void setAlamat(String alamat);
+    public void setTelp(String telp);
+    
+    public String getNama(int id);
+    public String getAlamat(int id);
+    public String getTelp(int id);
+}
